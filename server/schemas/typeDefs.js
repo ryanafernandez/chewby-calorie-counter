3dd4f9844fff758021283e29a8c43294eb43ea32
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-03-09 18:27:35 -0800

    Added starter code for User Model

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,28 +1,27 @@
 const { gql } = require('apollo-server-express');
 
-// const typeDefs = gql`
-//   type Tech {
-//     _id: ID!
-//     name: String!
-//   }
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+  }
 
-//   type Matchup {
-//     _id: ID!
-//     tech1: String!
-//     tech2: String!
-//     tech1_votes: Int
-//     tech2_votes: Int
-//   }
+  type Auth {
+    _id: ID!
+    user: User
+  }
 
-//   type Query {
-//     tech: [Tech]
-//     matchups(_id: String): [Matchup]
-//   }
+  type Query {
+    users: [User]
+    user(username: String!): User
+  }
 
-//   type Mutation {
-//     createMatchup(tech1: String!, tech2: String!): Matchup
-//     createVote(_id: String!, techNum: Int!): Matchup
-//   }
-// `;
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
 
 module.exports = typeDefs;
