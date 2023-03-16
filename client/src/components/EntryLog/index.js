f93ee56c75fb821fa5072184c25d4312c7074dc6
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-03-15 20:22:07 -0700

    Added pollInterval to useQuery to force refetch every 500ms

--- client/src/components/EntryLog/index.js
+++ client/src/components/EntryLog/index.js
@@ -6,13 +6,42 @@ import { REMOVE_ENTRY } from '../../utils/mutations'
 
 import Auth from '../../utils/auth';
 
+// Option 1 if you wanna pass around props
+// const EntryContent = () => {
+//     const [entries, setEntries] = Reacct.useState({})
+//     return(
+//         <div>
+//             <EntirLog entries={entries}></EntirLog>
+//             <EntryForm setEntries={setEntries} entries={entries}></EntryForm>
+//         </div>
+//     )
+// }
+
+// Option 2: refetching for new data
+// const EntryContent = () => {
+    // const { loading, error, data, refetch } = useQuery(QUERY_SINGLE_LOGGED_DAY, {
+    //     variables: { 
+    //         loggedDay: loggedDay, 
+    //         loggedDayAuthor: Auth.getProfile().data.username 
+    //     }
+    // });
+//     return(
+//         <div>
+//             <EntirLog loggedDayData={data.loggedDay}></EntirLog>
+//             <EntryForm onSubmit={refetch} ></EntryForm>
+//         </div>
+//     )
+// }
+
+
 const EntryLog = ({ loggedDay }) => {
    
-    const { loading, error, data } = useQuery(QUERY_SINGLE_LOGGED_DAY, {
+    const { loading, error, data, refetch } = useQuery(QUERY_SINGLE_LOGGED_DAY, {
         variables: { 
             loggedDay: loggedDay, 
             loggedDayAuthor: Auth.getProfile().data.username 
-        }
+        },
+        pollInterval: 500,
     });
 
     const [removeEntry, { removeError, removeData }] = useMutation(REMOVE_ENTRY);
@@ -21,7 +50,7 @@ const EntryLog = ({ loggedDay }) => {
     if (error) return console.error(error);
 
     const loggedDayData = data?.loggedDay;
-
+    
     // If no data logged for the day, say so.
     if (!loggedDayData || (loggedDayData.entries.length < 1)) {
         console.log("EntryLog - No logs found for:", loggedDay);
